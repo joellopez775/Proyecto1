b62bef03a0aa85bd188c28dbf736633c452499ad
BrandonWon <br.cayuela@duocuc.cl>
2024-01-10 20:16:25 -0300

    tkm

--- resta.js
+++ resta.js
@@ -1,6 +1,6 @@
 function resta(){
-    var v1=parseInt(documente.getElementById(valor1).value);
-    var v2=parseInt(documente.getElementById(valor2).value);
+    var v1=parseInt(document.getElementById("valor1").value);
+    var v2=parseInt(document.getElementById("valor2").value);
     var resta=0;
     resta=v1-v2;
 
