08bdc50e139ac2eef613ce0e357b6d9ac0a828e5
joellopez775 <joelignaciolopezbastias@gmail.com>
2024-01-10 19:43:06 -0300

    ejercicio suma

---
+++ suma.js
@@ -0,0 +1,9 @@
+
+function sumar(){
+    var v1=parseInt(documente.getElementById(valor1).value);
+    var v2=parseInt(documente.getElementById(valor2).value);
+    var suma=0;
+    suma=v1+v2;
+
+    document.getElementById("resultado").value=suma;
+}
