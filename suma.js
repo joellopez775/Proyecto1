b6cdbb96d44a8a2fc430dc7f93bae9f0ecc4ae56
BrandonWon <br.cayuela@duocuc.cl>
2024-01-10 21:29:33 -0300

    Avances con ejercicio

---
+++ suma.js
@@ -0,0 +1,8 @@
+function suma(){
+    var v1=parseInt(document.getElementById("valor1").value);
+    var v2=parseInt(document.getElementById("valor2").value);
+    var suma=0;
+    suma=v1+v2;
+
+    document.getElementById("resultado").value=suma;
+}
